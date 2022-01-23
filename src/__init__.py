0ef0bcfe953b39cdc357774a2a958a2e455450c6
CodeRonNor <ronh93@gmail.com>
2022-01-23 17:24:33 +0200

    notebook + results

--- src/__init__.py
+++ src/__init__.py
@@ -23,18 +23,18 @@ from src.vad_regressor.config import *
 
 # nltk.download('punkt')
 
-# # GPU settings
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# gpus = 1 if torch.cuda.is_available() else 0
-#
-# # Get model
-# trainer = pl.Trainer(gpus=gpus)
-# trained_model = Regressor.load_from_checkpoint(VAD_CKPT_PATH)
-# trained_model = trained_model.to(device)
-#
-# # Freeze
-# trained_model.eval()
-# trained_model.freeze()
-#
-# # Get tokenizer
-# tokenizer = RobertaTokenizer.from_pretrained(MODEL_NAME)
+# GPU settings
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+gpus = 1 if torch.cuda.is_available() else 0
+
+# Get model
+trainer = pl.Trainer(gpus=gpus)
+trained_model = Regressor.load_from_checkpoint(VAD_CKPT_PATH)
+trained_model = trained_model.to(device)
+
+# Freeze
+trained_model.eval()
+trained_model.freeze()
+
+# Get tokenizer
+tokenizer = RobertaTokenizer.from_pretrained(MODEL_NAME)
